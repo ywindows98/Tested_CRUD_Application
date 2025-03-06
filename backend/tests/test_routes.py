06104bf44dd1ab1f37717986cc6f19568d629623
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2025-03-06 18:06:03 +0100

    Configure testing with pytest

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,7 +1,15 @@
 import unittest
-from backend import app
+
+import sys
+sys.path.append('..')
+
+from backend.app import app
 
 class TestRoutes(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True # enable testing mode for flask
         self.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
