fc07d58d771f6a893694e61fdb859cd253d43821
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2025-03-13 19:06:01 +0100

    Add method to populate db with sample users for a showcase.

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 from src import create_app
-from src.populate import populate_subscriptions
+from src.populate import populate_subscriptions, populate_users
 
 app = create_app()
 populate_subscriptions(app)
+populate_users(app)
 
 if __name__ == '__main__':
 
--- backend/src/populate.py
+++ backend/src/populate.py
@@ -1,5 +1,7 @@
 from .database import db
-from .models import Subscription
+from .models import Subscription, User, StatusEnum
+import requests
+
 
 def populate_subscriptions(app):
     # Insert default subscription types if they don't exist
@@ -13,3 +15,40 @@ def populate_subscriptions(app):
 
         db.session.commit()
 
+def populate_users(app):
+    """Populate db with sample users fow a showcase"""
+    user1 = {
+        'username': 'PopulatedUser1',
+        'email': 'populated1@sample.com',
+        'subscription_id': 1,
+        'location': 'Kosice',
+        'status': StatusEnum.ONLINE.name
+    }
+
+    user2 = {
+        'username': 'PopulatedUser2',
+        'email': 'populated2@sample.com',
+        'subscription_id': 2,
+        'location': 'Bratislava',
+        'status': StatusEnum.OFFLINE.name
+    }
+
+    user3 = {
+        'username': 'PopulatedUser3',
+        'email': 'populated3@sample.com',
+        'subscription_id': 3,
+        'location': 'Presov',
+        'status': StatusEnum.ONLINE.name
+    }
+
+    users = [user1, user2, user3]
+
+    with app.app_context():
+        for user in users:
+            existing = User.query.filter_by(username=user['username']).first()
+            if not existing:
+                new_user = User(username=user['username'], email=user['email'], subscription_id=user['subscription_id'],
+                                location=user['location'], status=user['status'])
+                db.session.add(new_user)
+
+        db.session.commit()
